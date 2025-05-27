4f26f2ee9f32481c7a130cff4c5b512417a8c7ed
Shreyash Kant <108665847+Shreyash-kant@users.noreply.github.com>
2025-05-28 00:16:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 //your JS code here. If required.
+//your JS code here. If required.
+const table = document.getElementById("output");
+table.innerHTML += "<tr><td colspan=2>Loading...</td></tr>";
+const startTime = performance.now();
+function createPromise(label) {
+  return new Promise((res) => {
+    const delay = Math.random() * 2000 + 1000;
+    setTimeout(() => {
+      const timeTaken = (performance.now() - startTime) / 1000;
+      res({ label, timeTaken: parseFloat(timeTaken) });
+    }, delay);
+  });
+}
+
+Promise.all([
+  createPromise("Promise 1"),
+  createPromise("Promise 2"),
+  createPromise("Promise 3"),
+]).then((data) => {
+  table.innerHTML = "";
+  data.forEach(({ label, timeTaken }) => {
+    table.innerHTML += `<tr><td>${label}</td><td>${timeTaken.toFixed(
+      3
+    )}</td></tr>`;
+  });
+  const totalTime = Math.max(...data.map((t) => t.timeTaken));
+  table.innerHTML += `<tr><td>Total</td><td>${totalTime.toFixed(3)}</td></tr>`;
+});
